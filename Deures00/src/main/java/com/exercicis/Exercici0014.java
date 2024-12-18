6a2a45cfa41c127dfd573a78bd28a979882b1c23
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2024-12-18 12:49:33 +0100

    Exercici0014 en proceso

--- Deures00/src/main/java/com/exercicis/Exercici0014.java
+++ Deures00/src/main/java/com/exercicis/Exercici0014.java
@@ -8,42 +8,56 @@ import java.util.Locale;
 public class Exercici0014 {
 
     public static String getMovePC() {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return "";
+        String[] moves = {"PEDRA", "PAPER", "TISORES"};
+        return moves[new Random().nextInt(moves.length)];
     }
 
     public static String getPlayerMove(Scanner scanner) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return "";
+        while (true) {
+            System.out.print("Escriu PEDRA, PAPER, TISORES o SORTIR: ");
+            String move = scanner.nextLine().toUpperCase();
+
+            if (move.equals("SORTIR")) {
+                return null;
+            }
+            if (move.equals("PEDRA") || move.equals("PAPER") || move.equals("TISORES")) {
+                return move;
+            }
+        }
     }
 
     public static String getWinner(String playerMove, String pcMove) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return "";
+        if (playerMove.equals(pcMove)) {
+            return "DRAW" ;
+        }
+        switch (playerMove) {
+            case "PEDRA":
+                return pcMove.equals("TISORES") ? "PLAYER" : "PC";
+            case "PAPER":
+                return pcMove.equals("PEDRA") ? "PLAYER" : "PC";
+            case "TISORES":
+                return pcMove.equals("PAPER") ? "PLAYER" : "PC";
+            default:
+                return "ERROR";
+        }
     }
 
     public static String getScoreMessage(int playerScore, int pcScore) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return "";    }
+        return String.format("MARCADOR -> JUGADOR: %d - PC: %d", playerScore, pcScore);
+    }
 
     public static String getWinnerMessage(boolean playerWon) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return "";    }
+        return playerWon ? "Felicitats! Has guanyat la partida!" : "Ho sento! Ha guanyat el PC!";
+    }
 
     public static void updateStats(HashMap<String, Integer> stats, String move, boolean win) {
-        /*
-            TODO: Resol aquí la funció
-        */
+        String contador = move + "_COUNT";
+        stats.put(contador, stats.get(contador) + 1);
+
+        if (win) {
+            String winKey = move + "_WINS";
+            stats.put(winKey, stats.get(winKey) + 1);
+        }
     }
 
     public static String getMessage(String winner, String playerMove, String pcMove) {
